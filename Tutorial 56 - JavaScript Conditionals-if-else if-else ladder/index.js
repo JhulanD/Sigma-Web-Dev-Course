09417841b3b3a23b7d4b7af37a9af9ff3a50fc9a
Jhulan Dey <jhulandey@gmail.com>
2025-09-10 21:00:36 +0530

    Add example for JavaScript if-else conditionals
    Introduces a simple script demonstrating the use of if-else statements to check voting eligibility based on age. This serves as an example for JavaScript conditionals in tutorial 56.

---
+++ Tutorial 56 - JavaScript Conditionals-if-else if-else ladder/index.js
@@ -0,0 +1,13 @@
+// Tutorial 56 - JavaScript Conditionals-if-else if-else ladder
+
+console.log("Welcome to tutorial 56");
+console.log("Welcome to JavaScript Conditionals-if-else if-else ladder");
+
+let age = 25;
+if (age >= 18) {
+    console.log("You are eligible to vote");
+} else {
+    console.log("You are not eligible to vote");
+}
+
+// More examples
